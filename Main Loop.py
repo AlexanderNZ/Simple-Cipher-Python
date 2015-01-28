3ca3c23714ca7024cc6f197095ce315808a84e8c
Alex Corkin <alexcorkin@gmail.com>
2015-01-28 14:33:05 +1300

    added 2 radio buttons to control the state of the program

--- Main Loop.py
+++ Main Loop.py
@@ -17,16 +17,30 @@ def placeholderFunction():
 
 
 # Render GUI
-
-
 mainWindow = Tk()
-mainWindow.minsize(800, 600)
-mainWindow.geometry('800x600-50+50')
-mainWindow.mainloop()
+
 # Widgets
+
+# Program Title
+programTitle = StringVar()
+programTitle.set("Simple Cipher Program")
+titleWidget = Label(mainWindow, textvariable=programTitle)
+
+# User Action Radio Button
+
+radioController = IntVar()
+scramblerRadio = Radiobutton(mainWindow, text='Scramble a message', variable=radioController, value=1)
+unscrambleRadio = Radiobutton(mainWindow, text='Unscramble a message', variable=radioController, value=2)
 
 # Draws the window on screen complete with widgets
 
+mainWindow.minsize(800, 600)
+mainWindow.geometry('800x600-50+50')
+
+titleWidget.pack()
+scramblerRadio.pack()
+unscrambleRadio.pack()
+mainWindow.mainloop()
 
 #Get User passphrase
 #Encrypt Message --> Pass details to Encryption.py
