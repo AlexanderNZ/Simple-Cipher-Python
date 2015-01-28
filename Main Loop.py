dbceed3f5740344972726ef36d5a89cede4b4867
Alex Corkin <alexcorkin@gmail.com>
2015-01-28 13:57:43 +1300

    made the gui window resizable

--- Main Loop.py
+++ Main Loop.py
@@ -16,48 +16,18 @@ def placeholderFunction():
     button.pack()
 
 
+# Render GUI
 class GUI:
-    def __init__(self):
-        # Set Up User Menu
-        menubar = Menu(root)
-
-        filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Clear Data", command=placeholderFunction)
-
-        filemenu.add_separator()
-
-        filemenu.add_command(label="Exit", command=root.quit)
-        menubar.add_cascade(label="File", menu=filemenu)
-
-        contactMenu = Menu(menubar, tearoff=0)
-        contactMenu.add_command(label="4lex.nz Website", command=lambda aurl=personalWebsite: OpenUrl(aurl))
-        contactMenu.add_command(label="LinkedIn", command=lambda aurl=linkedIn: OpenUrl(aurl))
-        menubar.add_cascade(label="Contact", menu=contactMenu)
-
-        root.config(menu=menubar)
-
-        # Show title
-        titleMessage = StringVar()
-        titleMessage.set("Welcome to the Simple Cipher Program")
-
-        label = Label(root, textvariable=titleMessage, relief=RAISED, height=3)
-
-        label.pack()
+    def __init__(self, master):
+        master.minsize(width=666, height=666)
 
 
-        # Get User message
+mainWindow = Tk()
+mainWindow.mainloop()
+# Widgets
 
-        L1 = Label(root, text="User Name")
-        L1.pack(side=LEFT)
-        E1 = Entry(root, bd=5)
+# Draws the window on screen complete with widgets
 
-        E1.pack(side=RIGHT)
-
-
-if __name__ == '__main__':
-    root = Tk()
-    GUI(root)
-    root.mainloop()
 
 #Get User passphrase
 #Encrypt Message --> Pass details to Encryption.py
